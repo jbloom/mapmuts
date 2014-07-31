36e948b51d4d410ee11f6c17de617cbda5addb2f
jbloom <jbloom@fhcrc.org>
2014-07-31 13:59:45 -0700

    mapmuts_preferencemeans.py now handles differential preferences

--- scripts/mapmuts_preferencemeans.py
+++ scripts/mapmuts_preferencemeans.py
@@ -1,8 +1,8 @@
 #!python
 
-"""Computes mean of amino-acid preferences from several files.
+"""Computes mean of amino-acid preferences or differential preferences.
 
-Written by Jesse Bloom, 2013.
+Written by Jesse Bloom.
 """
 
 
@@ -51,32 +51,57 @@ def main():
     log.write("Read the following key/value pairs from infile %s:" % infilename)
     for (key, value) in d.iteritems():
         log.write("\n%s %s" % (key, value))
-    preferencefiles = mapmuts.io.ParseFileList(d, 'preferencefiles')
+    includestop = mapmuts.io.ParseBoolValue(d, 'includestop')
+    if 'preferencefiles' in d and 'differentialpreferencefiles' in d:
+        raise ValueError("Input file can only specify one of preferencefiles or differentialpreferencefiles, not both")
+    elif 'preferencefiles' in d:
+        use_diffprefs = False
+        preferencefiles = mapmuts.io.ParseFileList(d, 'preferencefiles')
+        log.write("\nReading in the preferences...")
+        log.flush()
+        preferences = [mapmuts.io.ReadEntropyAndEquilFreqs(f) for f in preferencefiles]
+        residues = preferences[0].keys()
+        residues.sort()
+        if includestop and ('PI_*' in preferences[0][residues[0]]):
+            includestop = True
+        else:
+            includestop = False
+            for x in preferences:
+                mapmuts.bayesian.PreferencesRemoveStop(x)
+    elif 'differentialpreferencefiles' in d:
+        use_diffprefs = True
+        preferencefiles = mapmuts.io.ParseFileList(d, 'differentialpreferencefiles')
+        log.write("\nReading in the differential preferences...")
+        log.flush()
+        preferences = [mapmuts.io.ReadDifferentialPreferences(f) for f in preferencefiles]
+        residues = preferences[0].keys()
+        residues.sort()
+        if includestop and ('dPI_*' in preferences[0][residues[0]]):
+            includestop = True
+        else:
+            includestop = False
+            for x in preferences:
+                mapmuts.bayesian.DifferentialPreferencesRemoveStop(x)
+    else:
+        raise ValueError("Input file failed to specify either preferencefiles or differentialpreferencefiles")
     nfiles = len(preferencefiles)
     if nfiles < 2:
-        raise ValueError('preferencefiles must specify at least 2 files')
+        raise ValueError('preferencefiles / differentialpreferencefiles must specify at least 2 files')
     outfile = mapmuts.io.ParseStringValue(d, 'outfile')
-    includestop = mapmuts.io.ParseBoolValue(d, 'includestop')
 
     # read the preferences
-    log.write("\nReading in the preferences...")
-    log.flush()
-    preferences = [mapmuts.io.ReadEntropyAndEquilFreqs(f) for f in preferencefiles]
     residues = preferences[0].keys()
     residues.sort()
     if not residues:
         raise ValueError("No residues specified in the first preference file")
-    if includestop and ('PI_*' in preferences[0][residues[0]]):
-        includestop = True
-    else:
-        includestop = False
-        for x in preferences:
-            mapmuts.bayesian.PreferencesRemoveStop(x)
     aas = mapmuts.sequtils.AminoAcids(includestop=includestop)
-    log.write("\nNow writing mean preferences to %s..." % outfile)
+    log.write("\nNow writing means to %s..." % outfile)
     log.flush()
     out = open(outfile, 'w')
-    out.write('#SITE\tWT_AA\tSITE_ENTROPY\t%s\n' % '\t'.join(['PI_%s' % aa for aa in aas]))
+    if use_diffprefs:
+        out.write('#SITE\tWT_AA\tRMS_dPI\t%s\n' % '\t'.join(['dPI_%s' % aa for aa in aas]))
+    else:
+        out.write('#SITE\tWT_AA\tSITE_ENTROPY\t%s\n' % '\t'.join(['PI_%s' % aa for aa in aas]))
     for r in residues:
         # get wildtype(s) for all files
         wts = [preferences[ifile][r]['WT_AA'] for ifile in range(nfiles)]
@@ -86,16 +111,24 @@ def main():
             wts = ','.join(wts)
         # check that all files are consistent with stop codon presence/absence
         for ifile in range(nfiles):
-            if includestop != ('PI_*' in preferences[ifile][r]):
-                raise ValueError("Not all files and residues are consistent with regard to the presence or absence of a key for stop codons (PI_*). All files and residues must either have or lack this key.")
+            if includestop != (('PI_*' in preferences[ifile][r]) or ('dPI_*' in preferences[ifile][r])):
+                raise ValueError("Not all files and residues are consistent with regard to the presence or absence of a key for stop codons (PI_* or dPI_*). All files and residues must either have or lack this key.")
         # start writing preference sums
-        meanpis = {}
+        means = {}
         for aa in aas:
-            meanpis[aa] = sum([preferences[ifile][r]['PI_%s' % aa] for ifile in range(nfiles)]) / float(nfiles)
-        if abs(1.0 - sum(meanpis.values())) > 1.0e-5:
-            raise ValueError("The mean preferences do not sum to nearly one for residue %d. Check that the preferences in the individual files correctly sum to one." % r)
-        h = Entropy(meanpis.values())
-        out.write("%d\t%s\t%g\t%s\n" % (r, wts, h, '\t'.join(['%g' % meanpis[aa] for aa in aas])))
+            if use_diffprefs:
+                means[aa] = sum([preferences[ifile][r]['dPI_%s' % aa] for ifile in range(nfiles)]) / float(nfiles)
+            else:
+                means[aa] = sum([preferences[ifile][r]['PI_%s' % aa] for ifile in range(nfiles)]) / float(nfiles)
+        if use_diffprefs and abs(sum(means.values())) > 1.0e-5:
+            raise ValueError("The mean differential preferences do not sum to nearly zero for residue %d. Check that the preferences in the individual files correctly sum to zero." % r)
+        elif not use_diffprefs and abs(1.0 - sum(means.values())) > 1.0e-5:
+            raise ValueError("The mean differential preferences do not sum to nearly one for residue %d. Check that the preferences in the individual files correctly sum to one." % r)
+        if use_diffprefs:
+            h = math.sqrt(sum([dpi**2 for dpi in means.values()]))
+        else:
+            h = Entropy(means.values())
+        out.write("%d\t%s\t%g\t%s\n" % (r, wts, h, '\t'.join(['%g' % means[aa] for aa in aas])))
     out.close()
     log.write("\n\nExecution completed at %s." % time.ctime())
 
